4bbf1824f40d2799fc7d9ebe9d095a9ff055ab93
Ayanokoji  Kiyotaka <33602166-sophiaTechInc@users.noreply.replit.com>
2025-02-28 15:07:24 +0000

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+function App() {
+    return (
+        <div className="flex justify-center items-center h-screen bg-gray-900 text-white">
+            <h1 className="text-4xl font-bold">Welcome to Aniphia!</h1>
+        </div>
+    )
+}
+export default App
---
+++ vite.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server:{
+    host:true,
+    port:3000,
+allowedHosts:['e914c37d-598d-45cb-a780-e7de4ba1e7b5-00-1cm4fz6vvt5az.janeway.replit.dev']
+
+  }
+    })
